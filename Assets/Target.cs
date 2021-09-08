62aa377e90be6088a4bd0caad598b5e890805f9f
Naushikha <naushikha.jayawickrama@gmail.com>
2021-09-08 22:32:45 +0530

    Added basic shooting and death

---
+++ Assets/Target.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void takeDamage(float amount){
+        health -= amount;
+        if (health <=0){
+            Die();
+        }
+    }
+
+    void Die(){
+        Destroy(gameObject);
+    }
+}
