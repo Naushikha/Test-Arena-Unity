c2787e477cedbb471de054ff6a874f41a6f110a7
Naushikha <naushikha.jayawickrama@gmail.com>
2021-09-09 00:40:02 +0530

    Added muzzle flash

--- Assets/gunScript.cs
+++ Assets/gunScript.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 public class gunScript : MonoBehaviour
 {
     public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
