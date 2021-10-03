962c5e8a4c62c70d1e3e1334346e99c6343430f6
Naushikha <naushikha.jayawickrama@gmail.com>
2021-10-03 21:22:48 +0530

    Goliath can now charge-up and fire

--- Assets/arena2/goliath/GoliathAI.cs
+++ Assets/arena2/goliath/GoliathAI.cs
@@ -7,22 +7,27 @@ namespace Goliath
 {
     public class GoliathAI : MonoBehaviour
     {
-        public float sightRange = 10f, attackRange = 5f, damageRange = 2f;
+        public float sightRange = 20f, fireRange = 15f, meleeRange = 2f;
         public float idleTime = 5.0f, patrolTime = 10.0f;
+        public float fireChargeTime = 1.0f, fireRate = 15f, fireTime = 5.0f;
         public float health = 100f;
+        public ParticleSystem fireRight;
+        public ParticleSystem fireLeft;
         protected internal Transform player;
         protected internal Terrain ground;
         protected internal Animator animator;
         protected internal NavMeshAgent agent;
+        protected internal GoliathSFX sfx;
         protected internal string waypointsName = "waypoints";
         protected internal List<Transform> waypoints = new List<Transform>();
         protected internal float animFadeIn = 0.5f;
-        protected internal bool playerInSightRange, playerInAttackRange, playerInDamageRange;
+        protected internal bool playerInSightRange, playerInFireRange, playerInMeleeRange;
         protected internal StateMachine stateMachine = new StateMachine();
         private void Start()
         {
             animator = GetComponent<Animator>();
             agent = GetComponent<NavMeshAgent>();
+            sfx = GetComponent<GoliathSFX>();
             player = GameObject.FindGameObjectWithTag("Player").transform;
             ground = GameObject.FindGameObjectWithTag("Ground").GetComponent<Terrain>();
             if (!player || !ground) Debug.LogError("Player or Ground is not set for Goliath!");
@@ -32,19 +37,24 @@ namespace Goliath
         }
         private void Update()
         {
-            float tmpPlayerDist = Vector3.Distance(transform.position, player.position);
-            playerInSightRange = tmpPlayerDist <= sightRange;
-            playerInAttackRange = tmpPlayerDist <= attackRange;
-            playerInDamageRange = tmpPlayerDist <= damageRange;
+            float distToPlayer = Vector3.Distance(transform.position, player.position);
+            playerInSightRange = distToPlayer <= sightRange;
+            playerInFireRange = distToPlayer <= fireRange;
+            playerInMeleeRange = distToPlayer <= meleeRange;
             stateMachine.Update();
         }
         public Vector3 getRandomWaypoint() { return waypoints[Random.Range(0, waypoints.Count)].position; }
+        public void playFireAnimation()
+        {
+            fireRight.Play();
+            fireLeft.Play();
+        }
         public void takeDamage(float amount) { }
         // private void Die() { stateMachine.ChangeState(new deadState(this)); }
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, attackRange);
+            Gizmos.DrawWireSphere(transform.position, fireRange);
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, sightRange);
         }
@@ -61,8 +71,8 @@ namespace Goliath
             timer += Time.deltaTime;
             if (timer > owner.idleTime) owner.stateMachine.ChangeState(new patrolState(owner));
             if (owner.playerInSightRange) owner.stateMachine.ChangeState(new chaseState(owner));
-            // if (owner.playerInSightRange && !owner.playerInAttackRange) owner.stateMachine.ChangeState(new chaseState(owner));
-            // if (owner.playerInAttackRange && owner.playerInSightRange) owner.stateMachine.ChangeState(new attackState(owner));
+            // if (owner.playerInSightRange && !owner.playerInFireRange) owner.stateMachine.ChangeState(new chaseState(owner));
+            // if (owner.playerInFireRange && owner.playerInSightRange) owner.stateMachine.ChangeState(new attackState(owner));
         }
         public void Exit() { }
     }
@@ -104,7 +114,58 @@ namespace Goliath
         {
             owner.agent.SetDestination(owner.player.position);
             if (!owner.playerInSightRange) owner.stateMachine.ChangeState(new idleState(owner));
+            if (owner.playerInFireRange) owner.stateMachine.ChangeState(new fireChargeState(owner));
         }
         public void Exit() { owner.agent.speed = prevSpeed; owner.agent.SetDestination(owner.agent.transform.position); }
+    }
+    public class fireChargeState : IState
+    {
+        GoliathAI owner;
+        public fireChargeState(GoliathAI owner) { this.owner = owner; }
+        private float timer = 0f;
+        public void Enter() { owner.animator.CrossFade("fireCharge", owner.animFadeIn); owner.sfx.fireCharge(); }
+        public void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > owner.fireChargeTime) owner.stateMachine.ChangeState(new fireState(owner));
+            owner.transform.LookAt(new Vector3(owner.player.position.x, owner.transform.position.y, owner.player.position.z));
+        }
+        public void Exit() { owner.sfx.fireChargeStop(); }
+    }
+    public class fireState : IState
+    {
+        GoliathAI owner;
+        public fireState(GoliathAI owner) { this.owner = owner; }
+        private float nextTimeToFire = 0f, timer = 0f;
+        public void Enter() { owner.animator.Play("fire"); }
+        public void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > owner.fireTime) owner.stateMachine.ChangeState(new fireDischargeState(owner));
+            if (Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / owner.fireRate;
+                owner.sfx.fire();
+                owner.playFireAnimation();
+            }
+            owner.transform.LookAt(new Vector3(owner.player.position.x, owner.transform.position.y, owner.player.position.z));
+        }
+        public void Exit() { }
+    }
+
+    public class fireDischargeState : IState
+    {
+        GoliathAI owner;
+        public fireDischargeState(GoliathAI owner) { this.owner = owner; }
+        private float timer = 0f;
+        public void Enter() { owner.animator.CrossFade("fireDischarge", owner.animFadeIn); }
+        public void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > owner.fireChargeTime) owner.stateMachine.ChangeState(new fireChargeState(owner));
+            owner.transform.LookAt(new Vector3(owner.player.position.x, owner.transform.position.y, owner.player.position.z));
+            if (!owner.playerInSightRange) owner.stateMachine.ChangeState(new idleState(owner));
+        }
+        public void Exit() { }
     }
 }
--- Assets/arena2/goliath/GoliathSFX.cs
+++ Assets/arena2/goliath/GoliathSFX.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class GoliathSFX : MonoBehaviour
 {
-    public AudioSource sfxPlaceStep, sfxStartStep;
-    void placeStep()
-    {
-        sfxPlaceStep.Play();
-    }
-    void startStep()
-    {
-        sfxStartStep.Play();
-    }
+    public AudioSource sfxStartStep, sfxPlaceStep, sfxFire;
+    public AudioClip clipFireCharge, clipFire;
+    public void placeStep() { sfxPlaceStep.Play(); }
+    public void startStep() { sfxStartStep.Play(); }
+    public void fireCharge() { sfxFire.PlayOneShot(clipFireCharge, 1.0f); }
+    public void fireChargeStop() { sfxFire.Stop(); }
+    public void fire() { sfxFire.PlayOneShot(clipFire, 0.7f); }
 }
